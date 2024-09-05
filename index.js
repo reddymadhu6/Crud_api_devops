cb0103a472d4590f2246e40e207f99835c7423d6
madhusoodan <madhusoodan.cs21@bmsce.ac.in>
2024-09-06 00:31:20 +0530

    update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app=express();
 
 app.get('/',(req,res)=>{
-    res.json({message:"Inside the container"});
+    res.json({message:"Inside the container v4"});
 })
 
 app.get('/health',(req,res)=>{
